95db4d8847da41b5a583d2c015de39150cadd5fa
ElezthemDev <97851451+Elezthem@users.noreply.github.com>
2023-09-28 08:35:32 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+/*void cmdprint(string gg) {
+	cout << gg << endl;
+}
+*/
+
+int addplus(int a, int b);
+int addplus2(int a, int b);
+
+int main() {
+	setlocale(LC_ALL, "RU");
+	
+	/*cmdprint("Hey");
+	string word = "Everyone";
+	cmdprint(word);
+	cmdprint("!");
+	*/
+
+	int res = addplus(10, 10);
+	int res2 = addplus2(40, 1);
+	if (res > res2)
+		cout << res;
+	else
+		cout << res2;
+
+	return 0;
+}
+
+int addplus(int a, int b) {
+	return a + b;
+}
+
+int addplus2(int a, int b) {
+	return a + b;
+}
